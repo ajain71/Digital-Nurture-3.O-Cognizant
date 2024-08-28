d9292a2d9d693e0c6b147f3338408451b1864816
aa6253 <94378420+ajain71@users.noreply.github.com>
2024-08-28 14:04:58 +0530

    Add files via upload

---
+++ Week 2_File IO/Exercise 1/readfile.java
@@ -0,0 +1,17 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderDemo {
+    public void readNoteFromFile(String fileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the note: " + e.getMessage());
+        }
+    }
+}
